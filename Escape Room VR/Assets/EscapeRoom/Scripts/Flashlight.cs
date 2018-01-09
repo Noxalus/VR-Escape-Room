db7e1714a5e1f576eae0cd20aeaf1f3b8462eaeb
Noxalus <noxalus@gmail.com>
2018-01-09 22:26:20 +0100

    Add a simple grabbable flashlight.

---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/Flashlight.cs
@@ -0,0 +1,37 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Flashlight : VRTK_InteractableObject
+    {
+        private VRTK_ControllerReference controllerReference;
+
+        public override void Grabbed(VRTK_InteractGrab grabbingObject)
+        {
+            base.Grabbed(grabbingObject);
+            controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+        }
+
+        public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+        {
+            base.Ungrabbed(previousGrabbingObject);
+            controllerReference = null;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            controllerReference = null;
+            interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed())
+            {
+                var hapticStrength = 10;
+                VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, hapticStrength, 0.5f, 0.01f);
+            }
+        }
+    }
+}
