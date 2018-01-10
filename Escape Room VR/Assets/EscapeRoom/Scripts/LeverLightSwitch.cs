ebdb7926d22d2319b13bcf4f546a688863f342ab
Noxalus <noxalus@gmail.com>
2018-01-10 16:38:27 +0100

    Add a lever to turn on/off the room light.

---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/LeverLightSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+public class LeverLightSwitch : MonoBehaviour {
+
+    private VRTK_Control_UnityEvents controlEvents;
+    public Light light;
+
+    private void Start()
+    {
+        controlEvents = GetComponent<VRTK_Control_UnityEvents>();
+        if (controlEvents == null)
+            controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
+
+        controlEvents.OnValueChanged.AddListener(HandleChange);
+    }
+
+    private void HandleChange(object sender, Control3DEventArgs e)
+    {
+        if (e.normalizedValue == 100)
+            light.enabled = true;
+        else if (e.normalizedValue == 0)
+            light.enabled = false;
+    }
+}
