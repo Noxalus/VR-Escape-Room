b87d2daf797b49f24b3575226b9804c54e7db967
Noxalus <noxalus@gmail.com>
2018-01-10 16:22:15 +0100

    Add a button to turn on/off the room light.

---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/LightSwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+public class LightSwitch : MonoBehaviour {
+    private VRTK_Button_UnityEvents buttonEvents;
+    private Light roomLight;
+
+    private void Start()
+    {
+        buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+        if (buttonEvents == null)
+            buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+
+        buttonEvents.OnPushed.AddListener(handlePush);
+        roomLight = GameObject.Find("RoomLight").GetComponent<Light>();
+    }
+
+    private void handlePush(object sender, Control3DEventArgs e)
+    {
+        VRTK_Logger.Info("Pushed");
+        roomLight.enabled = !roomLight.enabled;
+    }
+}
