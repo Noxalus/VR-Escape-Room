0291c3ab93368d709983a6fa9d1dc43c7639e1cb
Noxalus <noxalus@gmail.com>
2018-01-13 16:15:58 +0100

    Add a main menu.

---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/MainMenuActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class MainMenuActions : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        VRTK_Logger.Info("Play Button Clicked");
+        SceneManager.LoadScene("Room");
+    }
+
+    public void QuitButton()
+    {
+        VRTK_Logger.Info("Quit Button Clicked");
+        StartCoroutine(QuitGame());
+    }
+
+    private IEnumerator QuitGame()
+    {
+        yield return null;
+        Application.Quit();
+    }
+}
