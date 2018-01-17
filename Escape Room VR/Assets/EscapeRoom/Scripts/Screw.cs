f282c3eecb8582935cd5c99c0248ca9646ec9948
Noxalus <noxalus@gmail.com>
2018-01-17 13:13:09 +0100

    Add a script to handle screwed plate.

--- Escape Room VR/Assets/EscapeRoom/Scripts/Screw.cs
+++ Escape Room VR/Assets/EscapeRoom/Scripts/Screw.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
+using VRTK;
 
 public class Screw : MonoBehaviour {
 
-    private float screwOffset = 0.08f;
+    private ScrewedPlate screwPlate;
+
+    private float screwOffset = 0.075f;
     private bool unscrewed = false;
     private float speed = 0.1f;
+
+    protected void Start()
+    {
+        screwPlate = transform.parent.gameObject.GetComponent<ScrewedPlate>();
+    }
+
+    public bool IsScrewed()
+    {
+        return !unscrewed;
+    }
 
     public void Unscrew(float deltaTime)
     {
@@ -18,9 +31,13 @@ public class Screw : MonoBehaviour {
             gameObject.AddComponent<Rigidbody>();
 
             var rigidBody = gameObject.GetComponent<Rigidbody>();
-            rigidBody.AddTorque(0, Random.Range(-30f, 30f), Random.Range(-30f, 30f));
+            rigidBody.AddTorque(Random.Range(-60f, 60f), Random.Range(-60f, 60f), Random.Range(-60f, 60f));
+            rigidBody.AddForce(Random.Range(0f, 10f), 0f, 0f);
 
             unscrewed = true;
+            transform.SetParent(transform.parent.transform.parent.transform);
+
+            screwPlate.OnUnscrew(this);
         }
     }
 }
---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/ScrewedPlate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ScrewedPlate : MonoBehaviour {
+
+    private Screw[] Screws;
+    private bool free = false;
+
+	protected void Start ()
+    {
+        Screws = GetComponentsInChildren<Screw>();
+	}
+
+    public void OnUnscrew(Screw screw)
+    {
+        foreach (var s in Screws)
+        {
+            if (s.IsScrewed())
+                return;
+        }
+
+        // All screws have been unscrewed
+        Free();
+    }
+
+    private void Free()
+    {
+        if (free)
+            return;
+
+        var rigidBody = gameObject.AddComponent<Rigidbody>();
+        rigidBody.mass = 50f;
+
+        var interactableObject = gameObject.AddComponent<VRTK_InteractableObject>();
+        interactableObject.isGrabbable = true;
+        interactableObject.touchHighlightColor = Color.yellow;
+
+        free = true;
+    }
+}
