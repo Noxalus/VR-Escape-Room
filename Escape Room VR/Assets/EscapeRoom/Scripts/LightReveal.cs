9615a2dbdff19357716c5586dfb7afd23e5a4717
Noxalus <noxalus@gmail.com>
2018-02-07 20:52:40 +0100

    Add a shader to reveal hidden objects with a specific light.

---
+++ Escape Room VR/Assets/EscapeRoom/Scripts/LightReveal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightReveal : MonoBehaviour {
+
+    public Material lightReveal;
+    public Light light;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+        lightReveal.SetVector("_LightPosition", light.transform.position);
+        lightReveal.SetVector("_LightDirection", -light.transform.forward);
+        lightReveal.SetFloat("_LightAngle", light.spotAngle);
+    }
+}
